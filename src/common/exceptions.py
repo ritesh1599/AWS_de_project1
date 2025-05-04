585afcb61f1c062d3de272244a25442b29e6996f
ritesh1599 <ritesh.fupta.1599@gmail.com>
2025-05-04 16:31:38 +0530

    Created the directory! Add exceptions file

---
+++ src/common/exceptions.py
@@ -0,0 +1,11 @@
+class DataValidationError(Exception):
+    """Raised when data validation fails"""
+    pass
+
+class S3OperationError(Exception):
+    """Raised when S3 operations fail"""
+    pass
+
+class GlueJobError(Exception):
+    """Raised when Glue job fails"""
+    pass
